e202db6b4cf7b4a15ffc5836dfcf63f68d3b99a3
Arindam Podder <arindam@git>
2022-09-07 23:12:17 +0530

    react app 1

--- react_app1/src/component/LoginComponent.js
+++ react_app1/src/component/LoginComponent.js
@@ -1,12 +1,14 @@
 
 import React from "react";
-import "./LoginComponent.css";
-
+//import "./LoginComponent.css";
+import "../../node_modules/bootstrap/dist/css/bootstrap.css";
+import "../../node_modules/bootstrap-icons/font/bootstrap-icons.css";
 
 export function Login(){
     return (
        // <h1>user login</h1>
        <React.Fragment>
+       <div className="d-flex justify-content-center mt-4">
        <form>
        <h2>User Login</h2>
        <dl>
@@ -17,6 +19,7 @@ export function Login(){
        </dl>
        <button>Login</button>
        </form>
+       </div>
     </React.Fragment>
     );
 
